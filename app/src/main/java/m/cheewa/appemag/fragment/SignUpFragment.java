1d20f717909333097f9b40c47347573e6867622d
sodium <kikio_0@hotmail.com>
2017-09-08 15:56:56 +0700

    finish 8-9-17 new register

--- app/src/main/java/m/cheewa/appemag/fragment/SignUpFragment.java
+++ app/src/main/java/m/cheewa/appemag/fragment/SignUpFragment.java
@@ -3,10 +3,12 @@ package m.cheewa.appemag.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import m.cheewa.appemag.MainActivity;
 import m.cheewa.appemag.R;
 
 /**
@@ -22,5 +24,35 @@ public class SignUpFragment extends Fragment{
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fregment_sign_up, container, false);
         return view;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+        //ToolBar Controller
+        toolBarController();
+    }
+
+    private void toolBarController() {
+        Toolbar toolbar = getView().findViewById(R.id.toolbarSignUp);
+        ((MainActivity)getActivity()).getSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle(getResources().getString(R.string.new_register));
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+
+
     }
 }   // Main Class
