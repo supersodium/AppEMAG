ddc8225d33db84ee1c5b2961a51f813cb8f6e877
sodium <kikio_0@hotmail.com>
2017-09-08 14:15:00 +0700

    ui main ok

--- app/src/main/java/m/cheewa/appemag/MainActivity.java
+++ app/src/main/java/m/cheewa/appemag/MainActivity.java
@@ -3,11 +3,25 @@ package m.cheewa.appemag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import m.cheewa.appemag.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Add fragment to Activity
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+        }
+
+
+
+    }   // Main Method
+
+}   // Main Class
---
+++ app/src/main/java/m/cheewa/appemag/fragment/MainFragment.java
@@ -0,0 +1,26 @@
+package m.cheewa.appemag.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import m.cheewa.appemag.R;
+
+/**
+ * Created by User on 8/9/2560.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   //Main Class
